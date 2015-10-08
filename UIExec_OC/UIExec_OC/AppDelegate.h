1557ed3701236a70fb81400c082afdc02231a201
nxxjml <nxxjml@sina.cn>
2015-10-07 21:58:41 -0700

    project init

---
+++ UIExec_OC/UIExec_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIExec_OC
+//
+//  Created by simao on 15/10/7.
+//  Copyright © 2015年 simao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIExec_OC/UIExec_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIExec_OC
+//
+//  Created by simao on 15/10/7.
+//  Copyright © 2015年 simao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
